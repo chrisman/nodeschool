dd4ca7f57974c6549dda9ae44aba9650df7ea283
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-23 08:49:28 -0700

    learnyounode/08-collect

--- learnyounode/08-http-collect.js
+++ learnyounode/08-http-collect.js
@@ -1,9 +1,12 @@
 var http = require('http')
+var bl = require('bl')
 var data = [];
 
 http.get(process.argv[2], function(r){
-  r.setEncoding('utf8')
-  data += r
+  r.pipe(bl(function(e, d){
+    if (e) return console.error('err: ' + e);
+    d = d.toString()
+    console.log(d.length);
+    console.log(d);
+  }))
 });
-
-http.end()
