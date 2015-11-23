fdc2714700909befc20d7f0a35735c2ee1bb0cf3
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-22 19:17:26 -0700

    06-modules

--- learnyounode/06-module.js
+++ learnyounode/06-module.js
@@ -1,5 +1,17 @@
+var fs = require('fs')
+var path = require('path')
+
 module.exports = function(dir, ext, func){
-  var fs = require('fs')
-  var path = require('path')
-  
+  var matches = [];
+
+  fs.readdir(dir, function(e, d){
+    if (e) return func(e)
+    else {
+      for (var i in d){
+        if (path.extname(d[i]) === '.' + ext)
+          matches.push(d[i])
+      }
+      func(null, matches)
+    }
+  })
 }
--- learnyounode/06-program.js
+++ learnyounode/06-program.js
@@ -1,8 +1,8 @@
-var fs = require('fs')
 var ls = require('./06-module')
 
-fs.readdir(process.argv[2], function(e, d){
-  ls(process.argv[2], process.argv[3], function(e){
-
-  })
+ls(process.argv[2], process.argv[3], function(e, d) {
+  if (e) return console.error('error: ', e)
+  for (var i in d) {
+    console.log(d[i]);
+  }
 })
