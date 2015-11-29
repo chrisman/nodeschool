60a0e67ed9bbe032030b75964879576d43c2acd2
Christopher Brown <christopher.p.brown@gmail.com>
2015-11-29 08:32:07 -0700

    05-some-every.js

--- functional-javascript/05-some-every.js
+++ functional-javascript/05-some-every.js
@@ -1,11 +1,10 @@
 function checkUsersValid(goodUsers) {
   return function allUsersValid(submittedUsers) {
     // SOLUTION GOES HERE
-    submittedUsers.every(function(c){
-      // return (c.id in goodUsers);
-      // console.log(c.id + ' ... ' + goodUsers[c.id]);
-      if (c.id in goodUsers) return true
-      else return false
+    return submittedUsers.every(function(s){
+      return goodUsers.some(function(g){
+        return g.id === s.id
+      })
     })
   };
 }
